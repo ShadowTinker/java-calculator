322f444307ad7863f3bffe611acae25d37ee2275
shadowtinkery <shadowtinkery@gmail.com>
2020-05-31 19:48:13 +0800

    finish basic integer calculation 1. the input of 10 different digits 2. + - * / 3. clear and clear all

--- calculator.java
+++ calculator.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -8,6 +9,7 @@ import java.io.File;
 class MainFrame extends JFrame {
   private static final long serialVersionUID = 1L;
   // load icons
+  ImageIcon iconCalculator = new ImageIcon("assets/icons/calculator.png");
   ImageIcon iconClear = new ImageIcon("assets/icons/buttons/CE.png");
   ImageIcon iconClearAll = new ImageIcon("assets/icons/buttons/C.png");
   ImageIcon iconBackspace = new ImageIcon("assets/icons/buttons/backspace.png");
@@ -55,10 +57,17 @@ class MainFrame extends JFrame {
   // load customed fonts
   Font smallFont = null;
   Font bigFont = null;
-  // register components
+  // register components for output box
   JPanel outputBoxPanel = new JPanel();
-  JLabel result = new JLabel("1234567890", SwingConstants.RIGHT);
-  JLabel pending = new JLabel("1234567890", SwingConstants.RIGHT);
+  JLabel result = new JLabel("", SwingConstants.RIGHT);
+  JLabel pending = new JLabel("", SwingConstants.RIGHT);
+  // add some variables to implement the calculation function
+  String resultShow = "";
+  String pendingShow = "";
+  boolean isInitialized = false; // this is used to judge whether the first input is initialized
+  Double first = null; // this is the left operand of the equation
+  Double second = null; // this is the right operand of the equation
+  char lastOperator = '0';
   public void  addOutputBox () {
     try {
       smallFont = Font.createFont(Font.TRUETYPE_FONT, new File("assets/fonts/outputbox/ProFont For Powerline.ttf")).deriveFont(24f);
@@ -110,19 +119,544 @@ class MainFrame extends JFrame {
     buttonPanel.add(equal);
     add(buttonPanel, BorderLayout.CENTER);
   }
+  public void contanenatePending (String text) {
+    pending.setText(pending.getText() + text);
+  }
+  public void setResult (String text) {
+    result.setText(text);
+  }
+  class buttonOne implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 1.0;
+        } else {
+          first = first * 10 + 1;
+        }
+      } else {
+        if (second == null) {
+          second = 1.0;
+        } else {
+          second = second * 10 + 1;
+        }
+      }
+      contanenatePending("1");
+    }
+  }
+  class buttonTwo implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 2.0;
+        } else {
+          first = first * 10 + 2;
+        }
+      } else {
+        if (second == null) {
+          second = 2.0;
+        } else {
+          second = second * 10 + 2;
+        }
+      }
+      contanenatePending("2");
+    }
+  }
+  class buttonThree implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 3.0;
+        } else {
+          first = first * 10 + 3;
+        }
+      } else {
+        if (second == null) {
+          second = 3.0;
+        } else {
+          second = second * 10 + 3;
+        }
+      }
+      contanenatePending("3");
+    }
+  }
+  class buttonFour implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 4.0;
+        } else {
+          first = first * 10 + 4;
+        }
+      } else {
+        if (second == null) {
+          second = 4.0;
+        } else {
+          second = second * 10 + 4;
+        }
+      }
+      contanenatePending("4");
+    }
+  }
+  class buttonFive implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 5.0;
+        } else {
+          first = first * 10 + 5;
+        }
+      } else {
+        if (second == null) {
+          second = 5.0;
+        } else {
+          second = second * 10 + 5;
+        }
+      }
+      contanenatePending("5");
+    }
+  }
+  class buttonSix implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 6.0;
+        } else {
+          first = first * 10 + 6;
+        }
+      } else {
+        if (second == null) {
+          second = 6.0;
+        } else {
+          second = second * 10 + 6;
+        }
+      }
+      contanenatePending("6");
+    }
+  }
+  class buttonSeven implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 7.0;
+        } else {
+          first = first * 10 + 7;
+        }
+      } else {
+        if (second == null) {
+          second = 7.0;
+        } else {
+          second = second * 10 + 7;
+        }
+      }
+      contanenatePending("7");
+    }
+  }
+  class buttonEight implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 8.0;
+        } else {
+          first = first * 10 + 8;
+        }
+      } else {
+        if (second == null) {
+          second = 8.0;
+        } else {
+          second = second * 10 + 8;
+        }
+      }
+      contanenatePending("8");
+    }
+  }
+  class buttonNine implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 9.0;
+        } else {
+          first = first * 10 + 9;
+        }
+      } else {
+        if (second == null) {
+          second = 9.0;
+        } else {
+          second = second * 10 + 9;
+        }
+      }
+      contanenatePending("9");
+    }
+  }
+  class buttonZero implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 0.0;
+        } else {
+          first = first * 10;
+        }
+      } else {
+        if (second == null) {
+          second = 0.0;
+        } else {
+          second = second * 10;
+        }
+      }
+      contanenatePending("0");
+    }
+  }
+  class buttonPlus implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 0.0;
+        }
+        isInitialized = true;
+      } else {
+        if (second == null) {
+          if (lastOperator == '*') {
+            first = 0.0;
+            contanenatePending("0");
+          } else if (lastOperator == '/') {
+            setError("You cannot divide a 0");
+            return;
+          }
+        } else {
+          switch (lastOperator) {
+            case '+': {
+              first += second;
+              break;
+            }
+            case '-': {
+              first -= second;
+              break;
+            }
+            case '*': {
+              first *= second;
+              break;
+            }
+            case '/': {
+              if (Math.abs(second) < 0.0000001) {
+                setError("You cannot divide a 0");
+                return;
+              } else {
+                first /= second;
+              }
+              break;
+            }
+            default: {
+              first += second;
+              break;
+            }
+          }
+        }
+        second = null;
+        setResult(Double.toString(first));
+      }
+      lastOperator = '+';
+      contanenatePending("+");
+    }
+  }
+  class buttonMinus implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 0.0;
+        }
+        isInitialized = true;
+      } else {
+        if (second == null) {
+          if (lastOperator == '*') {
+            first = 0.0;
+            contanenatePending("0");
+          } else if (lastOperator == '/') {
+            setError("You cannot divide a 0");
+            return;
+          }
+        } else {
+          switch (lastOperator) {
+            case '+': {
+              first += second;
+              break;
+            }
+            case '-': {
+              first -= second;
+              break;
+            }
+            case '*': {
+              first *= second;
+              break;
+            }
+            case '/': {
+              if (Math.abs(second) < 0.0000001) {
+                setError("You cannot divide a 0");
+                return;
+              } else {
+                first /= second;
+              }
+              break;
+            }
+            default: {
+              first -= second;
+              break;
+            }
+          }
+        }
+        second = null;
+        setResult(Double.toString(first));
+      }
+      lastOperator = '-';
+      contanenatePending("-");
+    }
+  }
+  class buttonTimes implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 0.0;
+          contanenatePending("0");
+        }
+        isInitialized = true;
+      } else {
+        if (second == null) {
+          if (lastOperator == '*') {
+            first = 0.0;
+            contanenatePending("0");
+          } else if (lastOperator == '/') {
+            setError("You cannot divide a 0");
+            return;
+          }
+        } else {
+          switch (lastOperator) {
+            case '+': {
+              first += second;
+              break;
+            }
+            case '-': {
+              first -= second;
+              break;
+            }
+            case '*': {
+              first *= second;
+              break;
+            }
+            case '/': {
+              if (Math.abs(second) < 0.0000001) {
+                setError("You cannot divide a 0");
+                return;
+              } else {
+                first /= second;
+              }
+              break;
+            }
+            default: {
+              first *= second;
+              break;
+            }
+          }
+        }
+        second = null;
+        setResult(Double.toString(first));
+      }
+      lastOperator = '*';
+      contanenatePending("*");
+    }
+  }
+  class buttonOver implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 0.0;
+          contanenatePending("0");
+        }
+        isInitialized = true;
+      } else {
+        if (second == null) {
+          if (lastOperator == '*') {
+            first = 0.0;
+            contanenatePending("0");
+          } else if (lastOperator == '/') {
+            setError("You cannot divide a 0");
+            return;
+          }
+        } else {
+          switch (lastOperator) {
+            case '+': {
+              first += second;
+              break;
+            }
+            case '-': {
+              first -= second;
+              break;
+            }
+            case '*': {
+              first *= second;
+              break;
+            }
+            case '/': {
+              if (Math.abs(second) < 0.0000001) {
+                setError("You cannot divide a 0");
+                return;
+              } else {
+                first /= second;
+              }
+              break;
+            }
+            default: {
+              first /= second;
+              break;
+            }
+          }
+        }
+        second = null;
+        setResult(Double.toString(first));
+      }
+      lastOperator = '/';
+      contanenatePending("/");
+    }
+  }
+  class buttonEqual implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      if (!isInitialized) {
+        if (first == null) {
+          first = 0.0;
+          setResult("0");
+          contanenatePending("0");
+        }
+        isInitialized = true;
+      } else {
+        if (second == null) {
+          if (lastOperator == '*') {
+            first = 0.0;
+          } else if (lastOperator == '/') {
+            setError("You cannot divide a 0");
+          }
+          contanenatePending("0");
+        } else {
+          switch (lastOperator) {
+            case '+': {
+              first += second;
+              break;
+            }
+            case '-': {
+              first -= second;
+              break;
+            }
+            case '*': {
+              first *= second;
+              break;
+            }
+            case '/': {
+              if (Math.abs(second) < 0.0000001) {
+                setError("You cannot divide a 0");
+              } else {
+                first /= second;
+              }
+              break;
+            }
+            default: {
+              first /= second;
+              break;
+            }
+          }
+        }
+        second = null;
+        setResult(Double.toString(first));
+      }
+      pending.setText("");
+      second = null;
+      isInitialized = false;
+      lastOperator = '=';
+    }
+  }
+  class buttonClear implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      String current = pending.getText();
+      if (!isInitialized || lastOperator == '=') {
+        pending.setText("");
+        result.setText("");
+        first = null;
+        second = null;
+        isInitialized = false;
+        lastOperator = '0';
+      } else {
+        for (int j = current.length() - 1; j >= 0; j--) {
+          if (current.charAt(j) == '+') {
+            pending.setText(current.substring(0, j + 1));
+            second = null;
+            break;
+          } else if (current.charAt(j) == '-') {
+            pending.setText(current.substring(0, j + 1));
+            second = null;
+            break;
+          } else if (current.charAt(j) == '*') {
+            pending.setText(current.substring(0, j + 1));
+            second = null;
+            break;
+          } else if (current.charAt(j) == '/') {
+            pending.setText(current.substring(0, j + 1));
+            second = null;
+            break;
+          }
+        }
+      }
+    }
+  }
+  class buttonClearAll implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      pending.setText("");
+      result.setText("");
+      first = null;
+      second = null;
+      isInitialized = false;
+      lastOperator = '0';
+    }
+  }
+  public void setError (String content) {
+    pending.setText("");
+    result.setText("");
+    first = null;
+    second = null;
+    isInitialized = false;
+    lastOperator = '0';
+    int dialogType = JOptionPane.YES_OPTION;
+    String title = "Error!";
+    JOptionPane.showMessageDialog(null, content, title, dialogType);
+  }
+  public void bindEvents () {
+    one.addActionListener(new buttonOne());
+    two.addActionListener(new buttonTwo());
+    three.addActionListener(new buttonThree());
+    four.addActionListener(new buttonFour());
+    five.addActionListener(new buttonFive());
+    six.addActionListener(new buttonSix());
+    seven.addActionListener(new buttonSeven());
+    eight.addActionListener(new buttonEight());
+    nine.addActionListener(new buttonNine());
+    zero.addActionListener(new buttonZero());
+    plus.addActionListener(new buttonPlus());
+    minus.addActionListener(new buttonMinus());
+    times.addActionListener(new buttonTimes());
+    over.addActionListener(new buttonOver());
+    equal.addActionListener(new buttonEqual());
+    clear.addActionListener(new buttonClear());
+    clearAll.addActionListener(new buttonClearAll());
+  } 
   public MainFrame (String title) {
     super(title);
     // set basic attributes
     setBounds(500, 30, 530, 800);
     setResizable(false);
+    setIconImage(iconCalculator.getImage());
     // Add components
     addOutputBox();
     addButtons();
+    bindEvents();
     setVisible(true);
   }
 }
 public class calculator {
   public static void main(String[] args) {
-    new MainFrame("Calculator");
+    new MainFrame("My Own Java Calculator");
   }
 }
